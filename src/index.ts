a4c568c65b052db0043631e503921064ad421884
Unchaptered <workstation19961002@gmail.com>
2021-11-23 20:33:18 +0900

    Class on TS
    React, Express, Node 등을 사용하면,
    Class 를 사용 해야 한다.
    
    그러나 Interface 가 TS 측면에서는 더 안정적이다.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human{
-    name:string,
-    age:number,
-    gender:string
+class Human {
+    public age: number;
+    public name: string;
+    public gender: string;
+    constructor(name: string, age:number, gender:string){
+        this.age=age;
+        this.name=name;
+        this.gender=gender;
+    }
 }
-const person={
-    name: "unchaptered",
-    age:22,
-    gender: "male"
-}
+
+const annonymouse=new Human("Unchaptered",18,"male");
+
 const sayHello=(person: Human):string=>{
-    // console.log(`Hello ${name}, you are ${age} and you are a ${gender}`);
     return `Hello ${person.name}, you are ${person.age} and you are a ${person.gender}`;
 }
 
-
-console.log(sayHello(person));
+console.log(sayHello(annonymouse));
 
 export {};
