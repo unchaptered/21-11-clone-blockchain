2f6c4299a211d0c89eb6b9188d8c0a9979d46d86
Unchaptered <workstation19961002@gmail.com>
2021-11-23 12:10:40 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("hello");
+// tsconfig.json 설정
+// compilerOptions 컴파일 옵션 설정
+// module : 일반적인 js?
+// target : 어떤 버전의 js?
+// sourceMap : sourcemap 처리 유무?
+// include : 컴파일 과정에서 포함할 파일
+// exclude : 
+//# sourceMappingURL=index.js.map
