a8a5ae149bdd54971cb1f6d4a211cb0a05958dac
Unchaptered <workstation19961002@gmail.com>
2021-11-23 21:07:10 +0900

    Block Chain on TS

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
-    public index:number;
-    public hash:string;
-    public previousHash:string;
-    public data:string;
-    public timestamp:number;
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
 
-    static calculateBlockHash=(
-        index:number,
-        previousHash:string,
-        timestamp:string,
-        data:string
-    ):string=>CryptoJS.SHA256(index+previousHash+timestamp+data).toString();
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string =>CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
     constructor(
         index:number,
@@ -32,10 +32,31 @@ class Block {
 const genesisBlock:Block=new Block(0,"2020202020202","","Hello",123456);
 
 let blockchain:Block[]=[genesisBlock];
-// TS 의 기능이 오직 Block 클래스만 blockchain 에 추가하도록 제어한다.
 
 const getBlockchain=():Block[]=>blockchain;
 const getLatestBlock=():Block=>blockchain[blockchain.length-1];
 const getNewTimeStamp=():number=>Math.round(new Date().getTime() / 1000);
+
+const createNewBlock=(data:string):Block =>{
+    const previousBlock:Block=getLatestBlock();
+    const newIndex:number=previousBlock.index+1;
+    const newTimeStamp:number=getNewTimeStamp();
+    const newHash:string=Block.calculateBlockHash(
+        newIndex,
+        previousBlock.hash,
+        newTimeStamp,
+        data
+    );
+    const newBlock:Block=new Block(
+        newIndex,
+        newHash,
+        previousBlock.hash,
+        data,
+        newTimeStamp
+    );
+    return newBlock;
+};
+
+console.log(createNewBlock("hello"),createNewBlock("bye bye"));
 
 export {};
