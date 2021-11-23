8ffffd0cbe8b3420b6b21613643b143b8d0dd406
Unchaptered <workstation19961002@gmail.com>
2021-11-23 20:50:32 +0900

    Block Chain on TS
    0.9 Creating BLock part Two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index:number;
     public hash:string;
     public previousHash:string;
     public data:string;
     public timestamp:number;
+
+    static calculateBlockHash=(
+        index:number,
+        previousHash:string,
+        timestamp:string,
+        data:string
+    ):string=>CryptoJS.SHA256(index+previousHash+timestamp+data).toString();
+
     constructor(
         index:number,
         hash:string,
@@ -21,8 +31,11 @@ class Block {
 
 const genesisBlock:Block=new Block(0,"2020202020202","","Hello",123456);
 
-let blockchain:[Block]=[genesisBlock];
+let blockchain:Block[]=[genesisBlock];
 // TS 의 기능이 오직 Block 클래스만 blockchain 에 추가하도록 제어한다.
-console.log(blockchain);
+
+const getBlockchain=():Block[]=>blockchain;
+const getLatestBlock=():Block=>blockchain[blockchain.length-1];
+const getNewTimeStamp=():number=>Math.round(new Date().getTime() / 1000);
 
 export {};
